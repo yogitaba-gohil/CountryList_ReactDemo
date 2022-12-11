f912dba4e46a33e602fc5199502ab9f0d03b0e25
Yogitaba Gohil <yogitabagohil1212@gmail.com>
2022-12-11 21:53:27 +0200

    basic structure of countrypage done

--- src/Pages/CountryDetails.js
+++ src/Pages/CountryDetails.js
@@ -1,40 +1,67 @@
-import React, {useEffect, useState} from 'react'
-import {
-    useParams
-  } from "react-router-dom";
-  import {getCountry} from '../services/countryDetails'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getCountry } from "../services/countryDetails";
+import PlaceIcon from '@mui/icons-material/Place';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 
-const CountryDetails = ()  => {
-    const { name } = useParams();
-    const[countryDetails,setCountryDetails] = useState([])
 
-    useEffect(() => {
-      if(name) {
-       const post = getCountry(name).then( data => { 
-         return data;
-      });
-      console.log('post', post)
-      setCountryDetails(post);
-      } 
-      console.log('countryDetails', countryDetails)
-  
-}, [])
-  
+const CountryDetails = () => {
+  const { name } = useParams();
+  const [countryDetails, setCountryDetails] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      if (name) {
+        const post = await getCountry(name).then((data) => {
+          console.log("data ", data);
+          return data;
+        });
+        setCountryDetails(post);
+      }
+    }
+    fetchData();
+    console.log("first", countryDetails);
+  });
+
   return (
     <div>
+      <div class="container">
+        <nav class="navbar navbar-expand-lg">
+          <div class="container-fluid">
+            <img src="..." class="rounded-circle bg-primary" alt="..." />
 
-  <div class="container">
-  <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-      <img src="#"  alt="profile"/>
-      <h1></h1>
-    </div>
-  </nav>
-</div>
-    </div>
-   
-  )
-}
+            <div class="d-flex flex-column mb-3">
+              <div class="p-2">AFGHANISTAN</div>
+              <div class="p-2">Kabul</div>
+            </div>
+            <div>
+              <MoreVertIcon />
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div class="card" style={{ width: "100%", textAlign:"center" }}>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <div className="d-flex " style={{textAlign:"start" }} >  
+          <ArrowBackIosNewIcon />
+          
 
-export default CountryDetails
+          <PlaceIcon />
+          
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountryDetails;
